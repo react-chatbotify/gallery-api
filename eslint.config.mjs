9b46efff02f2be1737fc433fb7b44297555e137f
Bashamega <adambashaahmednaji@gmail.com>
2024-08-25 13:58:27 +0300

    patch: eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,30 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
 
 export default [
-  { files: ["**/*.{js,mjs,cjs,ts}"] },
-  { files: ["**/*.js"], languageOptions: { sourceType: "script" } },
-  { languageOptions: { globals: globals.browser } },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    languageOptions: {
+      parser: tsParser,
+      sourceType: "module",
+      globals: globals.browser,
+    },
+    // Include the recommended configurations directly
+    plugins: {
+      "@typescript-eslint": tseslint,
+    },
+    rules: {
+      ...pluginJs.configs.recommended.rules,
+      ...tseslint.configs.recommended.rules,
+      "@typescript-eslint/no-explicit-any": "off", // Disable the no-explicit-any rule
+    },
+  },
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      sourceType: "script",
+    },
+  },
 ];
