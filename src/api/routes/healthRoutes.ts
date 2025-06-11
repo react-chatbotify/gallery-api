d7d2c0404a43ecf66c1b52cc5ed43f16f21c4c95
tjtanjin <cjtanjin@gmail.com>
2025-06-12 00:46:14 +0800

    ci: Update ci

--- src/api/routes/healthRoutes.ts
+++ src/api/routes/healthRoutes.ts
@@ -3,6 +3,7 @@ import { getHealthStatus } from '../controllers/healthController';
 
 const router = Router();
 
+// todo: should improve health logic
 router.get('/healthz', getHealthStatus);
 
 export default router;
