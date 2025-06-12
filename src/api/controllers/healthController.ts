ec65a0be5f133256a4d1b36a94e75acf85cc2341
tjtanjin <cjtanjin@gmail.com>
2025-06-13 00:58:36 +0800

    chore: Minor cleanup

--- src/api/controllers/healthController.ts
+++ src/api/controllers/healthController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 
 // todo: maybe build a separate lightweight status service as well
+// should also consider rag service
 export const getHealthStatus = (req: Request, res: Response) => {
   res.status(200).json({ status: 'healthy' });
 };
