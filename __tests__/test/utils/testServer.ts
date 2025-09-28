401eade8a392dd471b9489620f607de12611dcb3
Farrel Athaillah <MoonLMC1212@gmail.com>
2025-09-28 16:14:44 +0000

    resolve the problem

--- __tests__/test/utils/testServer.ts
+++ __tests__/test/utils/testServer.ts
@@ -1,9 +1,5 @@
 import express, { type RequestHandler } from 'express';
-
-// IMPORTANT: this import is replaced below with your actual path
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-import userRoutes from '../../src/api/routes/userRoutes.ts';
+import userRoutes from '../../../src/api/routes/userRoutes';
 
 export const BASE = `/api/${process.env.API_VERSION || 'v1'}/users`;
 
@@ -15,15 +11,12 @@ export const makeApp = () => {
   const app = express();
   app.use(express.json());
   app.use(BASE, userRoutes);
-  // No string path here; avoids path-to-regexp '*' issues in Express 5
   app.use(notFound);
   return app;
 };
 
 export const http = () => {
   const app = makeApp();
-  // lazy require to keep types optional
-  // eslint-disable-next-line @typescript-eslint/no-var-requires
   const request = require('supertest') as typeof import('supertest');
   return request(app);
 };
